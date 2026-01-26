a5c10e7b3b7ab2a79d3932afdff65de3bad041e8
keita <keitaorg@gmail.com>
2026-01-26 06:10:23 -0400

    reestruturado

--- back-end/internal/handlers/clientes.go
+++ back-end/internal/handlers/clientes.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"database/sql"
 	"embed"
-	"html/template"
 	"net/http"
+	"strconv"
 )
 
 
@@ -21,16 +21,22 @@ type Cliente struct {
 
 
 func SetupRoutes(db *sql.DB, templates embed.FS) {
- tmpl := template.Must(template.New("").ParseFS(templates, "frontend/templates/**/*.html"))
-
+  Init(templates, db)
  //Clientes
- http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	 tmpl.ExecuteTemplate(w, "index.html", nil)
- })
+ http.HandleFunc("/", Homepage)
+// DB = db
+ http.HandleFunc("/clientes", GetClients) 
 
- http.HandleFunc("/clientes", func(w http.ResponseWriter, r *http.Request) {
+ http.HandleFunc("/clientes", PostClients) 
+
+ http.HandleFunc("/clientes/", FilterClients)
+
+ http.HandleFunc("/clientes/", PutClients)
+}
+
+func GetClients(w http.ResponseWriter, r *http.Request) {
 	 if r.Method == "GET" {
-		 rows, err := db.Query("Select * from clientes order by clinetes.nome")
+		 rows, err := DB.Query("Select * from clientes order by clinetes.nome")
 		 if err != nil {
 			 http.Error(w, err.Error(), http.StatusInternalServerError)
 			 return
@@ -48,20 +54,85 @@ func SetupRoutes(db *sql.DB, templates embed.FS) {
 			 clientes = append(clientes, c)
 		 }
 		 
-		  tmpl.ExecuteTemplate(w,"clientes/lista.html", clientes)
-
-	 } else if r.Method == "POST" {
-		 nome := r.FormValue("nome")
-		 email := r.FormValue("email")
-     telefone := r.FormValue("telefone")
-		 cpfcnpj := r.FormValue("cpf_cnpj")
-
-		 _, err := db.Exec("INSERT INTO clientes (nome, email, telefone, cpf_cnpj) VALUES(?,?,?,?)", 
-		 nome, email, telefone, cpfcnpj)
-		 if err != nil {
-			 http.Error(w, err.Error(), http.StatusInternalServerError)
-			 return
-		 }
+		  Tmpl.ExecuteTemplate(w,"clientes/lista.html", clientes)
 	 }
- })
 }
+
+func PostClients(w http.ResponseWriter, r *http.Request) {
+		 if r.Method == "POST" {
+			 
+			 nome := r.FormValue("nome")
+			 email := r.FormValue("email")
+			 telefone := r.FormValue("telefone")
+			 cpfcnpj := r.FormValue("cpf_cnpj")
+
+			 _, err := DB.Exec("INSERT INTO clientes (nome, email, telefone, cpf_cnpj) VALUES(?,?,?,?)", 
+			 nome, email, telefone, cpfcnpj)
+			 if err != nil {
+				 http.Error(w, err.Error(), http.StatusInternalServerError)
+				 return
+			 }
+			 w.Header().Set("HX-Refresh", "true")
+			 w.WriteHeader(http.StatusCreated)
+		 }
+
+}
+
+func FilterClients(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Path[len("/clientes/"):]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "ID invalido!", http.StatusBadRequest)
+		return
+	}
+	if r.Method == "GET" {
+		var c Cliente
+		err :=  DB.QueryRow("Select * from clientes where id = ?", id).
+	  Scan(&c.ID, &c.Nome, &c.Email, &c.Telefone, &c.Cpf_cnpj, &c.DataCadastro, &c.UsuarioCadastro)
+		if err !=  nil {
+	    http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		Tmpl.ExecuteTemplate(w, "clientes/formulario.html", c)
+  }
+}
+
+
+func PutClients(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Path[len("/clientes/"):]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	if r.Method == "PUT" {
+
+			 nome := r.FormValue("nome")
+			 email := r.FormValue("email")
+			 telefone := r.FormValue("telefone")
+			 cpfcnpj := r.FormValue("cpf_cnpj")
+
+			 _, err := DB.Exec("UPDATE clientes SET nome=?, email=?, telefone=?, cpf_cnpj=?) where id=?", 
+			 nome, email, telefone, cpfcnpj, id)
+			 if err != nil {
+				 http.Error(w, err.Error(), http.StatusInternalServerError)
+				 return
+			 }
+			 w.Header().Set("HX-Refresh", "true")
+			 w.WriteHeader(http.StatusCreated)
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
